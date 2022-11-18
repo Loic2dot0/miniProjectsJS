fbd5ea167a5f66eea666dcf7dc862461341fbf7f
Loic2dot0 <moreaux.loic@gmail.com>
2022-11-18 17:37:59 +0100

    js: création de la liste des caractères création de la fonction addCharactersSet déclaration de la constante CHARACTERS_SET

--- password-generator/js/app.js
+++ password-generator/js/app.js
@@ -5,3 +5,19 @@ const INPUT_CHECKBOX = [...document.querySelectorAll('input[type="checkbox"')];
 const BTN_COPY = document.querySelector('.btn-copy');
 
 RANGE_DISPLAY.textContent = `Taille du mot de passe : ${INPUT_RANGE.value}`;
+
+// charactersSet=[lowercase, uppercase, number. special]
+const CHARACTERS_SET = [
+    addCharactersSet(97, 122),
+    addCharactersSet(65, 90),
+    addCharactersSet(48, 57),
+    addCharactersSet(33, 47) + addCharactersSet(58, 64) + addCharactersSet(91, 96) + addCharactersSet(123, 126)
+];
+
+function addCharactersSet(fromCode, toCode){
+    let charactersList = '';
+    for(let i= fromCode; i <= toCode; i++){
+        charactersList += String.fromCharCode(i);
+    }
+    return charactersList;
+}
