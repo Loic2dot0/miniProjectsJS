178447f3ff5e9aadad0caa4b7c45d72f8f78f891
Loic2dot0 <moreaux.loic@gmail.com>
2022-11-21 17:56:31 +0100

    js: création de la fonction displayDurationTime ajout de l'event Listener loadeddata création de la fonction convertSecondesToHHMMSS

--- video-player/js/app.js
+++ video-player/js/app.js
@@ -2,7 +2,7 @@ const VIDEO = document.querySelector('video');
 const BTN = {
     play: document.querySelector('.btn-play'),
     mute: document.querySelector('.btn-mute'),
-    fullscren: document.querySelector('.btn-fullscreen'),
+    fullscreen: document.querySelector('.btn-fullscreen'),
     volume: document.querySelector('input[type="range"]')
 };
 const TIME = {
@@ -14,3 +14,19 @@ const CONTROLS = {
     isMute: false,
     isFullscreen: false
 };
+
+VIDEO.addEventListener('loadeddata', displayDurationTime);
+
+function displayDurationTime(){
+    TIME.duration.textContent = convertSecondesToHHMMSS(VIDEO.duration);
+}
+
+function convertSecondesToHHMMSS(timeInSecondes){
+    let hours = Math.floor(timeInSecondes / 3600);
+    let minutes = Math.floor((timeInSecondes - hours * 3600)/ 60);
+    let secondes = Math.floor(timeInSecondes - hours * 3600 - minutes * 60);
+    
+    if(minutes < 10) minutes = `0${minutes}`;
+    if(secondes < 10) secondes = `0${secondes}`;
+    return hours == 0 ? `${minutes}:${secondes}` : `${hours}:${minutes}:${secondes}`;
+}
