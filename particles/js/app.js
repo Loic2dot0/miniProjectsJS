9e9844955ce638abfada5d06494c925b9d732f8d
Loic2dot0 <moreaux.loic@gmail.com>
2022-12-07 16:45:39 +0100

    js: modification de la classe Particle

--- particles/js/app.js
+++ particles/js/app.js
@@ -13,4 +13,22 @@ class Particle {
         this.size = size,
         this.color = color
     }
+    draw(){
+        CTX.beginPath();
+        CTX.arc(this.x, this.y, this.size, 0, Math.PI * 2, true);
+        CTX.fillStyle = this.color;
+        CTX.fill();
+    }
+    update(){
+        if(this.x < 0 || this.x > CANVAS.width){
+            this.directionX = -this.directionX;
+        }
+        if(this.y < 0 || this.y > CANVAS.height){
+            this.directionY = -this.directionY;
+        }
+        
+        this.x += this.directionX;
+        this.y += this.directionY;
+        this.draw();
+    }
 }
