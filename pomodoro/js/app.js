e47201d8b4615fbe7d7d3cf25f1cf6eb33ed49cc
Loic2dot0 <moreaux.loic@gmail.com>
2022-12-09 16:56:23 +0100

    js: création de la fonction init création de la constante TIMER_INIT modification de la constante COUNTER

--- pomodoro/js/app.js
+++ pomodoro/js/app.js
@@ -5,9 +5,26 @@ const DISPLAY = {
 }
 const BTN_PLAY = document.querySelector('.btn-play');
 const BTN_RESET = document.querySelector('.btn-reset');
+const TIMER_INIT = {
+    work: 1800, //1800s = 30minutes
+    rest: 300 //300s = 5minutes
+}
 const COUNTER = {
-    work: 1800,  //1800s = 30minutes
-    rest: 300, //300s = 5minutes
+    work: 0,  
+    rest: 0, 
     cycle: 0
 }
 let play = false;
+
+init();
+
+function init(){
+    COUNTER.work = TIMER_INIT.work;
+    COUNTER.rest = TIMER_INIT.rest;
+    COUNTER.cycle = 0;
+    play = false;
+
+    DISPLAY.workCounter.textContent = COUNTER.work;
+    DISPLAY.restCounter.textContent = COUNTER.rest;
+    DISPLAY.cycle.textContent = COUNTER.cycle;
+}
