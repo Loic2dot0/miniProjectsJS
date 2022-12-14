a7908c38249edceadc16a970896180ef733e31f2
Loic2dot0 <moreaux.loic@gmail.com>
2022-12-14 16:35:08 +0100

    js: création de la fonction identifyInput

--- form-validation/js/app.js
+++ form-validation/js/app.js
@@ -1,5 +1,5 @@
 const FORM = document.forms[0];
-const INPUTS = document.querySelectorAll('input');
+const INPUTS = [...document.querySelectorAll('input')];
 const VALIDATION_ICONS = document.querySelectorAll('.validation-icon');
 const ERROR_MESSAGES = document.querySelectorAll('.error-message');
 const INPUTS_VALIDITY = {
@@ -32,24 +32,28 @@ function showValidation(index, validation, message){
     ERROR_MESSAGES[index].textContent = message;
 }
 
-INPUTS.forEach((input, indexInput) => {
-    input.addEventListener('input', () => {
-        switch(indexInput){
-            case 0 :
-                verifyInputUser(input.value, indexInput);
-                break;
-            case 1 :
-                verifyInputMail(input.value, indexInput);
-                break;
-            case 2 :
-                verifyInputPassword(input.value, indexInput);
-                break;
-            case 3 :
-                verifyInputPasswordConfirm(input.value, indexInput);
-                break;    
-        }
-    });
+INPUTS.forEach(input => {
+    input.addEventListener('input', identifyInput);
+    input.addEventListener('blur', identifyInput);
 });
+
+function identifyInput(e){
+    let indexInput = e.target ? INPUTS.indexOf(e.target) : e;
+    switch(indexInput){
+        case 0 :
+            verifyInputUser(indexInput);
+            break;
+        case 1 :
+            verifyInputMail(indexInput);
+            break;
+        case 2 :
+            verifyInputPassword(indexInput);
+            break;
+        case 3 :
+            verifyInputPasswordConfirm(indexInput);
+            break;    
+    }
+}
 
 function verifyInputUser(userValue, indexInput){
     INPUTS_VALIDITY.user = userValue.length < 3 ? false : true;
